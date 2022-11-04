616c0048b075621acbaff5bf0d9d448fc3050c64
lucianor <regueraluciano@gmail.com>
2022-11-04 00:02:21 -0300

    Create Array

--- 02-sintaxis/index.js
+++ 02-sintaxis/index.js
@@ -1,14 +1,6 @@
-const name = 'Luciano Reguera';
-
-const age = 41;
-
-const isDev = true;
-
-const birthday = '24/11/1980'
-
-const book = {
+const list = ['Luciano Reguera', 41, true, new Date(24, 11, 1980), {
     title: 'My favorite Book',
     author: 'My favorite Author',
     date: '01/01/1970',
     url: 'www.any.com'
-};
+}];
