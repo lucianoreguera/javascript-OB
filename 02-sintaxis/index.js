553803ebb03072b4eaf19150c1058d588e6d6698
lucianor <regueraluciano@gmail.com>
2022-11-03 23:58:22 -0300

    Ejercicio 2 - Sintaxis - JS - OpenBootcamp

---
+++ 02-sintaxis/index.js
@@ -0,0 +1,14 @@
+const name = 'Luciano Reguera';
+
+const age = 41;
+
+const isDev = true;
+
+const birthday = '24/11/1980'
+
+const book = {
+    title: 'My favorite Book',
+    author: 'My favorite Author',
+    date: '01/01/1970',
+    url: 'www.any.com'
+};
