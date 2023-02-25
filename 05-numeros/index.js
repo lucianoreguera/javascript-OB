1b1500250be104203ac16c205839b15bab657546
lucianor <regueraluciano@gmail.com>
2023-02-25 12:39:15 -0300

    Ejercicio 5 - Numeros en JS - JS - OpenBootcamp

---
+++ 05-numeros/index.js
@@ -0,0 +1,14 @@
+const alturaCm = 186
+const alturaMts = 1.86
+const pesoKg = 104.5
+const alturaMtsRedondeadoArriba = Math.round(alturaMts)
+const pesoKgRedondeadoAbajo = Math.floor(pesoKg)
+const maximoValorJS = Number.MAX_VALUE + 1
+
+console.log(alturaCm)
+console.log(alturaMts)
+console.log(pesoKg)
+console.log(alturaMtsRedondeadoArriba)
+console.log(pesoKgRedondeadoAbajo)
+console.log(maximoValorJS)
+
